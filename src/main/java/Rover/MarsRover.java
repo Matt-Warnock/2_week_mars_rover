df578e1315b9f460daacbb2da757eb141edf1c4f
timcastillogill <timgillviola@gmail.com>
2022-09-12 16:54:28 +0100

    Added rotator class

--- src/main/java/Rover/MarsRover.java
+++ src/main/java/Rover/MarsRover.java
@@ -1,25 +1,21 @@
 package Rover;
 
 public class MarsRover {
-    private String currantDirection = "N";
+    private Rotator rotator;
+
+    public MarsRover() {
+        this.rotator = new Rotator("N");
+    }
+
     public String execute(String moveInstructions) {
-        int numberOfMoves = 0;
-        String[] directionPositions = {"N", "E", "S", "W"};
+//        int numberOfMoves = 0;
+//        String[] directionPositions = {"N", "E", "S", "W"};
 
-        if (moveInstructions.contains("M")) {
-            numberOfMoves = moveInstructions.length();
-        }
+//        if (moveInstructions.contains("M")) {
+//            numberOfMoves = moveInstructions.length();
+//        }
 
-        Rotator rotator = new Rotator("N");
-//        if (moveInstructions.equals("R")) {
-//            direction = directionPositions[1];
-//        }
-//        if (moveInstructions.equals("RR")) {
-//            direction = directionPositions[2];
-//        }
-//        if (moveInstructions.equals("RRR")) {
-//            direction = directionPositions[3];
-//        }
+        this.rotator.turnMarsRover(moveInstructions);
 
         return "0:" + numberOfMoves + ":" + direction;
     }
--- src/main/java/Rover/Rotator.java
+++ src/main/java/Rover/Rotator.java
@@ -1,16 +1,30 @@
 package Rover;
 
-public class Rotator {
+import java.util.Arrays;
 
-    private String currantDirection;
+public class Rotator {
+    private String currentDirection;
     String[] directionPositions = {"N", "E", "S", "W"};
 
-
-    public Rotator(String currantDirection) {
-        this.currantDirection = currantDirection;
+    public String getCurrentDirection() {
+        return currentDirection;
     }
 
-    public String turnRight() {
-        
+    public Rotator(String currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+
+    public void turnRight() {
+        int indexOfCurrentDirection = currentDirection.indexOf(Arrays.toString(directionPositions));
+        currentDirection =  directionPositions[indexOfCurrentDirection + 1];
+    }
+
+    public void turnMarsRover(String moveInstructions) {
+        Rotator rotator = new Rotator(currentDirection);
+        for (int i = 0; i < moveInstructions.length(); i++) {
+            if (moveInstructions.charAt(i) == ('R')) {
+                rotator.turnRight();
+            }
+        }
     }
 }
