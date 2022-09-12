d849b92076890fb1a94bc3e70f0e87ec6c6b19e6
adrixo <adrianvalrom@gmail.com>
2022-09-12 14:47:54 +0200

    green: rotator should rotate Right

--- src/main/java/Rotator.java
+++ src/main/java/Rotator.java
@@ -1,5 +1,18 @@
 public class Rotator {
-    public String turn(String r, int i) {
-        return "E";
+
+    private int indexOfDirection;
+    String[] directionPositions = {"N", "E", "S", "W"};
+
+
+    public Rotator(String currentDirection) {
+        for (int i = 0; i < directionPositions.length; i++) {
+            if (directionPositions[i] == currentDirection) {
+                indexOfDirection = i;
+            }
+        }
+    }
+
+    public String turn(String r, int numberOfRotations) {
+        return directionPositions[(indexOfDirection + numberOfRotations) % directionPositions.length];
     }
 }
--- src/test/java/RotatorShould.java
+++ src/test/java/RotatorShould.java
@@ -4,15 +4,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RotatorShould {
+    private String originalDirection = "N";
 
     @Test
     public void rotate_right_when_rotated_right_once() {
-        assertEquals("E", new Rotator().turn("R", 1) );
+        assertEquals("E", new Rotator(originalDirection).turn("R", 1) );
     }
 
     @Test
     public void rotate_right_twice_when_rotated_right_twice() {
-        assertEquals("S", new Rotator().turn("R", 2) );
+        assertEquals("S", new Rotator(originalDirection).turn("R", 2) );
     }
 
+    @Test
+    public void rotate_right_twice_when_rotated_right_five_times() {
+        assertEquals("E", new Rotator(originalDirection).turn("R", 5) );
+    }
 }
